1b63440de8a24eb551d59b7a4b58968793b15a1d
Sung Kim <sungjk77@msn.com>
2021-05-02 23:18:22 -0700

    added an update button

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -15,6 +15,27 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+//update
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = Blog.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!blogData) {
+      res.status(404).json({ message: 'No blog found with this id!' });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const blogData = await Blog.destroy({
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,5 @@
+// const { Blog } = require('../../models');
+
 const newFormHandler = async (event) => {
   event.preventDefault();
 
@@ -21,8 +23,19 @@ const newFormHandler = async (event) => {
   }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
+const ButtonHandler = async (event) => {
+  let action = "";
+  if (event.target.hasAttribute('id')) {
+    action = event.target.getAttribute('id');
+  }
+  if (event.target.hasAttribute('data-name') && action=="update") {
+    document.getElementById("updateModalTitle").defaultValue = event.target.getAttribute('data-name');
+    document.getElementById("updateModalText").defaultValue = event.target.getAttribute('data-message');
+    document.getElementById("updateID").defaultValue = event.target.getAttribute('data-id');
+  }
+
+  //delete portion
+  if (event.target.hasAttribute('data-id') && action=="delete") {
     const id = event.target.getAttribute('data-id');
 
     const response = await fetch(`/api/blogs/${id}`, {
@@ -35,6 +48,29 @@ const delButtonHandler = async (event) => {
       alert('Failed to delete blog');
     }
   }
+
+};
+
+const updateFormHandler = async (event) => {
+  event.preventDefault();
+  const name = document.querySelector('#updateModalTitle').value.trim();
+  const description = document.querySelector('#updateModalText').value.trim();
+  const id = document.querySelector('#updateID').value.trim();
+
+  if (name && description) {
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ name, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to update blog');
+    }
+  }
 };
 
 document
@@ -43,4 +79,8 @@ document
 
 document
   .querySelector('.blog-list')
-  .addEventListener('click', delButtonHandler);
+  .addEventListener('click', ButtonHandler);
+
+document
+  .querySelector('.update-form')
+  .addEventListener('click', updateFormHandler);
